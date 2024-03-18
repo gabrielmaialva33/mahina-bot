f9d6babe32e0e4f0e48123b556e7671c21e3934e
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-03-18 02:55:11 -0300

    chore: linting code

--- src/exceptions/missing_permissions.exception.ts
+++
@@ -1,9 +0,0 @@
-export class MissingPermissionsException {
-  message = 'Missing permissions:'
-
-  constructor(public permissions: string[]) {}
-
-  toString() {
-    return `${this.message} ${this.permissions.join(', ')}`
-  }
-}
