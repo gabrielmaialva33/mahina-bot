eb23090bf0f97daf6cedc9913e7e2c8b93956998
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-03-18 00:22:43 -0300

    feat: add knex and configure

--- knexfile.mjs
+++ knexfile.mjs
@@ -1,13 +1,14 @@
 import { SQLITE_CONFIG } from './build/database/sqlite.config.js'
+import path from 'node:path';
 
 export default {
   ...SQLITE_CONFIG,
   migrations: {
     ...SQLITE_CONFIG.migrations,
-    directory: './build/database/migrations'
+    directory: path.join(process.cwd(), 'build', 'database', 'migrations')
   },
   seeds: {
     ...SQLITE_CONFIG.seeds,
-    directory: './build/database/seeds'
+    directory: path.join(process.cwd(), 'build', 'database', 'seeds')
   }
 }
