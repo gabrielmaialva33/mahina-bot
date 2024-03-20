3431dd18aa7724f25482665821da0493a089c138
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-03-20 00:26:43 -0300

    feat: add more commnads

---
+++ src/commands/music/remove.ts
@@ -0,0 +1,72 @@
+import { Command, Context, Mahina } from '#common/index'
+
+export default class Remove extends Command {
+  constructor(client: Mahina) {
+    super(client, {
+      name: 'remove',
+      description: {
+        content: 'Remove uma música da fila',
+        examples: ['remove 1'],
+        usage: 'remove <song number>',
+      },
+      category: 'music',
+      aliases: ['rm'],
+      cooldown: 3,
+      args: true,
+      player: {
+        voice: true,
+        dj: true,
+        active: true,
+        dj_perm: null,
+      },
+      permissions: {
+        dev: false,
+        client: ['SendMessages', 'ViewChannel', 'EmbedLinks'],
+        user: [],
+      },
+      slashCommand: true,
+      options: [
+        {
+          name: 'song',
+          description: 'O número da música que você deseja remover',
+          type: 4,
+          required: true,
+        },
+      ],
+    })
+  }
+  async run(client: Mahina, ctx: Context, args: string[]): Promise<any> {
+    const player = client.queue.get(ctx.guild!.id)
+    const embed = this.client.embed()
+    if (!player.queue.length)
+      return await ctx.sendMessage({
+        embeds: [embed.setColor(this.client.color.red).setDescription('𝙉𝙖̃𝙤 𝙝𝙖́ 𝙢𝙪́𝙨𝙞𝙘𝙖𝙨 𝙣𝙖 𝙛𝙞𝙡𝙖.')],
+      })
+    if (Number.isNaN(Number(args[0])))
+      return await ctx.sendMessage({
+        embeds: [
+          embed.setColor(this.client.color.red).setDescription('𝙄𝙨𝙨𝙤 𝙣𝙖̃𝙤 𝙚́ 𝙪𝙢 𝙣𝙪́𝙢𝙚𝙧𝙤 𝙫𝙖́𝙡𝙞𝙙𝙤.'),
+        ],
+      })
+    if (Number(args[0]) > player.queue.length)
+      return await ctx.sendMessage({
+        embeds: [
+          embed.setColor(this.client.color.red).setDescription('𝙄𝙨𝙨𝙤 𝙣𝙖̃𝙤 𝙚́ 𝙪𝙢 𝙣𝙪́𝙢𝙚𝙧𝙤 𝙫𝙖́𝙡𝙞𝙙𝙤.'),
+        ],
+      })
+    if (Number(args[0]) < 1)
+      return await ctx.sendMessage({
+        embeds: [
+          embed.setColor(this.client.color.red).setDescription('𝙄𝙨𝙨𝙤 𝙣𝙖̃𝙤 𝙚́ 𝙪𝙢 𝙣𝙪́𝙢𝙚𝙧𝙤 𝙫𝙖́𝙡𝙞𝙙𝙤.'),
+        ],
+      })
+    player.remove(Number(args[0]) - 1)
+    return await ctx.sendMessage({
+      embeds: [
+        embed
+          .setColor(this.client.color.main)
+          .setDescription(`𝙈𝙪́𝙨𝙞𝙘𝙖 ${Number(args[0])} 𝙧𝙚𝙢𝙤𝙫𝙞𝙙𝙖 𝙙𝙖 𝙛𝙞𝙡𝙖.`),
+      ],
+    })
+  }
+}
---
+++ src/commands/music/resume.ts
@@ -0,0 +1,46 @@
+import { Command, Context, Mahina } from '#common/index'
+
+export default class Resume extends Command {
+  constructor(client: Mahina) {
+    super(client, {
+      name: 'resume',
+      description: {
+        content: 'Continua a música que está pausada',
+        examples: ['resume'],
+        usage: 'resume',
+      },
+      category: 'music',
+      aliases: ['r'],
+      cooldown: 3,
+      args: false,
+      player: {
+        voice: true,
+        dj: false,
+        active: true,
+        dj_perm: null,
+      },
+      permissions: {
+        dev: false,
+        client: ['SendMessages', 'ViewChannel', 'EmbedLinks'],
+        user: [],
+      },
+      slashCommand: true,
+      options: [],
+    })
+  }
+  async run(client: Mahina, ctx: Context): Promise<any> {
+    const player = client.queue.get(ctx.guild!.id)
+    const embed = this.client.embed()
+    if (!player.paused)
+      return await ctx.sendMessage({
+        embeds: [
+          embed.setColor(this.client.color.red).setDescription('𝙊 𝙥𝙡𝙖𝙮𝙚𝙧 𝙣𝙖̃𝙤 𝙚𝙨𝙩𝙖́ 𝙥𝙖𝙪𝙨𝙖𝙙𝙤.'),
+        ],
+      })
+    player.pause()
+
+    return await ctx.sendMessage({
+      embeds: [embed.setColor(this.client.color.main).setDescription(`𝙍𝙚𝙩𝙤𝙢𝙖𝙣𝙙𝙤 𝙖 𝙢𝙪́𝙨𝙞𝙘𝙖.. 🎶`)],
+    })
+  }
+}
