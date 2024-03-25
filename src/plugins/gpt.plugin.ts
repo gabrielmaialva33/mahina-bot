0f5829ed01b0bec24f61c34aaab9660d287c1431
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-03-25 16:53:25 -0300

    fix: rxjs in user service

--- src/plugins/gpt.plugin.ts
+++ src/plugins/gpt.plugin.ts
@@ -13,7 +13,7 @@ export class AI extends OpenAI {
   private config = {
     model: 'gpt-3.5-turbo-instruct',
     //model: 'text-davinci-002',
-    temperature: 0.8,
+    temperature: 0.7,
     max_tokens: 128,
     frequency_penalty: 0.3,
     presence_penalty: 0.5,
