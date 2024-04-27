51c14ccc8403a847203d4cbabf11187e66cc11ce
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-04-26 22:46:06 -0300

    chore: linting code

--- src/plugins/gpt.plugin.ts
+++ src/plugins/gpt.plugin.ts
@@ -13,7 +13,7 @@ export class AI extends OpenAI {
   private config = {
     model: 'gpt-3.5-turbo-instruct',
     //model: 'text-davinci-002',
-    temperature: 0.7,
+    temperature: 0.8,
     max_tokens: 256,
     frequency_penalty: 0.6,
     presence_penalty: 0.7,
