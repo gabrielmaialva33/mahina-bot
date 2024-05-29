4a803188c7534b883147d8985b8b2c29774d47b2
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-05-29 15:56:50 -0300

    refactor: base entity

--- src/plugins/gpt.plugin.ts
+++ src/plugins/gpt.plugin.ts
@@ -13,9 +13,9 @@ export class AI extends OpenAI {
   private config = {
     model: 'gpt-3.5-turbo-instruct',
     //model: 'text-davinci-002',
-    temperature: 0.9,
+    temperature: 1,
     max_tokens: 568,
-    frequency_penalty: 0.5,
+    frequency_penalty: 1,
     presence_penalty: 1,
     n: 1,
     stop: ['||'],
