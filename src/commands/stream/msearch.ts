ac7dce71ad910e4e2a0747f7a2e8286942aa63db
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-06-06 17:03:12 -0300

    chore: bump version

--- src/commands/stream/msearch.ts
+++ src/commands/stream/msearch.ts
@@ -61,151 +61,105 @@ export default class MSearch extends Command {
 
     const numbersEmojis = ['1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣']
 
-    try {
-      const fetchPage = async (pageIndex: number) => {
-        if (cache.pages[pageIndex]) {
-          return cache.pages[pageIndex]
-        }
-
-        const nextPageToken = cache.nextPageTokens[pageIndex - 1] || null
-        const response: SearchResponse | null = await client.animezey.searchAnime(
-          search,
-          nextPageToken
-        )
-        if (!response) return null
-
-        const { data } = response
-        const filteredData = {
-          ...data,
-          files: data.files.filter((file) => file.mimeType === 'video/x-matroska'),
-        }
-        cache.pages[pageIndex] = filteredData
-        cache.nextPageTokens[pageIndex] = data.nextPageToken
-        return filteredData
+    // try {
+    const fetchPage = async (pageIndex: number) => {
+      if (cache.pages[pageIndex]) {
+        return cache.pages[pageIndex]
       }
 
-      // Function to create embed for a page
-      const createEmbed = (
-        pageData: any[],
-        pageIndex: number,
-        itemPageIndex: number,
-        totalItems: number
-      ) => {
-        const itemsPerPage = 5
-        const startIndex = itemPageIndex * itemsPerPage
-        const paginatedData = pageData.slice(startIndex, startIndex + itemsPerPage)
+      const nextPageToken = cache.nextPageTokens[pageIndex - 1] || null
+      const response: SearchResponse | null = await client.animezey.searchAnime(
+        search,
+        nextPageToken
+      )
+      if (!response) return null
 
-        const embed = new EmbedBuilder()
-          .setTitle(`**__Resultados da Pesquisa__**: ${search}`)
-          .setDescription(
-            paginatedData
-              .map(
-                (anime, index) => `
+      const { data } = response
+      const filteredData = {
+        ...data,
+        files: data.files.filter((file) => file.mimeType === 'video/x-matroska'),
+      }
+      cache.pages[pageIndex] = filteredData
+      cache.nextPageTokens[pageIndex] = data.nextPageToken
+      return filteredData
+    }
+
+    // Function to create embed for a page
+    const createEmbed = (
+      pageData: any[],
+      pageIndex: number,
+      itemPageIndex: number,
+      totalItems: number
+    ) => {
+      const itemsPerPage = 5
+      const startIndex = itemPageIndex * itemsPerPage
+      const paginatedData = pageData.slice(startIndex, startIndex + itemsPerPage)
+
+      const embed = new EmbedBuilder().setTitle(`**__Resultados da Pesquisa__**: ${search}`)
+      if (paginatedData.length === 0)
+        return embed.setDescription('Nenhum resultado encontrado').setColor(client.color.red)
+
+      embed
+        .setDescription(
+          paginatedData
+            .map(
+              (anime, index) => `
               **Número**: ${numbersEmojis[index]}
               **Nome**: ${anime.name}
               **Tamanho**: ${(Number.parseInt(anime.size) / 1024 / 1024 / 1024).toFixed(2)} GB
               **Data de Modificação**: ${moment(anime.modifiedTime).format('DD/MM/YYYY HH:mm:ss')}
               **Link**: [Download](${client.animezey.BASE_URL + anime.link})
               `
-              )
-              .join('\n\n')
-          )
-          .setFooter({
-            text: `Página ${pageIndex + 1} | Itens ${startIndex + 1}-${startIndex + paginatedData.length} de ${totalItems}`,
-            iconURL: ctx.author!.avatarURL() || undefined,
-          })
-          .setColor(client.color.violet)
-
-        return embed
-      }
-
-      // Function to handle pagination with download buttons
-      const handlePage = async (pageIndex: number, itemPageIndex: number, interaction: any) => {
-        const pageData = await fetchPage(pageIndex)
-        if (!pageData)
-          return interaction.update({ content: 'Erro ao buscar informações', components: [] })
-
-        const totalItems = pageData.files.length
-        const embed = createEmbed(pageData.files, pageIndex, itemPageIndex, totalItems)
-
-        const itemsPerPage = 5
-        const startIndex = itemPageIndex * itemsPerPage
-
-        const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
-          new ButtonBuilder()
-            .setCustomId('prevPage')
-            .setLabel('Página Anterior')
-            .setStyle(ButtonStyle.Primary)
-            .setDisabled(pageIndex === 0),
-          new ButtonBuilder()
-            .setCustomId('nextPage')
-            .setLabel('Próxima Página')
-            .setStyle(ButtonStyle.Primary)
-            .setDisabled(!cache.nextPageTokens[pageIndex]),
-          new ButtonBuilder()
-            .setCustomId('prevItemPage')
-            .setLabel('Itens Anteriores')
-            .setStyle(ButtonStyle.Secondary)
-            .setDisabled(itemPageIndex === 0),
-          new ButtonBuilder()
-            .setCustomId('nextItemPage')
-            .setLabel('Próximos Itens')
-            .setStyle(ButtonStyle.Secondary)
-            .setDisabled((itemPageIndex + 1) * itemsPerPage >= totalItems)
+            )
+            .join('\n\n')
         )
+        .setFooter({
+          text: `Página ${pageIndex + 1} | Itens ${startIndex + 1}-${startIndex + paginatedData.length} de ${totalItems}`,
+          iconURL: ctx.author!.avatarURL() || undefined,
+        })
+        .setColor(client.color.violet)
 
-        const downloadRow = new ActionRowBuilder<ButtonBuilder>()
-        for (let i = 0; i < Math.min(itemsPerPage, totalItems - startIndex); i++) {
-          downloadRow.addComponents(
-            new ButtonBuilder()
-              .setCustomId(`download_${i + 1}`)
-              .setLabel(`Assistir ${numbersEmojis[i]}`)
-              .setStyle(ButtonStyle.Success)
-          )
-        }
+      return embed
+    }
 
-        await interaction.update({ embeds: [embed], components: [row, downloadRow] })
-      }
+    // Function to handle pagination with download buttons
+    const handlePage = async (pageIndex: number, itemPageIndex: number, interaction: any) => {
+      const pageData = await fetchPage(pageIndex)
+      if (!pageData)
+        return interaction.update({ content: 'Erro ao buscar informações', components: [] })
 
-      const initialPageIndex = 0
-      const initialItemPageIndex = 0
-      const initialPageData = await fetchPage(initialPageIndex)
-      if (!initialPageData) return ctx.sendMessage('Erro ao buscar informações')
+      const totalItems = pageData.files.length
+      const embed = createEmbed(pageData.files, pageIndex, itemPageIndex, totalItems)
 
-      const totalItems = initialPageData.files.length
-      const initialEmbed = createEmbed(
-        initialPageData.files,
-        initialPageIndex,
-        initialItemPageIndex,
-        totalItems
-      )
+      const itemsPerPage = 5
+      const startIndex = itemPageIndex * itemsPerPage
 
-      const initialRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      const row = new ActionRowBuilder<ButtonBuilder>().addComponents(
         new ButtonBuilder()
           .setCustomId('prevPage')
           .setLabel('Página Anterior')
           .setStyle(ButtonStyle.Primary)
-          .setDisabled(true),
+          .setDisabled(pageIndex === 0),
         new ButtonBuilder()
           .setCustomId('nextPage')
           .setLabel('Próxima Página')
           .setStyle(ButtonStyle.Primary)
-          .setDisabled(!cache.nextPageTokens[initialPageIndex]),
+          .setDisabled(!cache.nextPageTokens[pageIndex]),
         new ButtonBuilder()
           .setCustomId('prevItemPage')
           .setLabel('Itens Anteriores')
           .setStyle(ButtonStyle.Secondary)
-          .setDisabled(true),
+          .setDisabled(itemPageIndex === 0),
         new ButtonBuilder()
           .setCustomId('nextItemPage')
           .setLabel('Próximos Itens')
           .setStyle(ButtonStyle.Secondary)
-          .setDisabled(totalItems <= 5)
+          .setDisabled((itemPageIndex + 1) * itemsPerPage >= totalItems)
       )
 
-      const initialDownloadRow = new ActionRowBuilder<ButtonBuilder>()
-      for (let i = 0; i < Math.min(5, totalItems); i++) {
-        initialDownloadRow.addComponents(
+      const downloadRow = new ActionRowBuilder<ButtonBuilder>()
+      for (let i = 0; i < Math.min(itemsPerPage, totalItems - startIndex); i++) {
+        downloadRow.addComponents(
           new ButtonBuilder()
             .setCustomId(`download_${i + 1}`)
             .setLabel(`Assistir ${numbersEmojis[i]}`)
@@ -213,6 +167,63 @@ export default class MSearch extends Command {
         )
       }
 
+      await interaction.update({ embeds: [embed], components: [row, downloadRow] })
+    }
+
+    const initialPageIndex = 0
+    const initialItemPageIndex = 0
+    const initialPageData = await fetchPage(initialPageIndex)
+    if (!initialPageData) return ctx.sendMessage('Erro ao buscar informações')
+
+    const totalItems = initialPageData.files.length
+    const initialEmbed = createEmbed(
+      initialPageData.files,
+      initialPageIndex,
+      initialItemPageIndex,
+      totalItems
+    )
+
+    const initialRow = new ActionRowBuilder<ButtonBuilder>().addComponents(
+      new ButtonBuilder()
+        .setCustomId('prevPage')
+        .setLabel('Página Anterior')
+        .setStyle(ButtonStyle.Primary)
+        .setDisabled(true),
+      new ButtonBuilder()
+        .setCustomId('nextPage')
+        .setLabel('Próxima Página')
+        .setStyle(ButtonStyle.Primary)
+        .setDisabled(!cache.nextPageTokens[initialPageIndex]),
+      new ButtonBuilder()
+        .setCustomId('prevItemPage')
+        .setLabel('Itens Anteriores')
+        .setStyle(ButtonStyle.Secondary)
+        .setDisabled(true),
+      new ButtonBuilder()
+        .setCustomId('nextItemPage')
+        .setLabel('Próximos Itens')
+        .setStyle(ButtonStyle.Secondary)
+        .setDisabled(totalItems <= 5)
+    )
+
+    const initialDownloadRow = new ActionRowBuilder<ButtonBuilder>()
+    for (let i = 0; i < Math.min(5, totalItems); i++) {
+      initialDownloadRow.addComponents(
+        new ButtonBuilder()
+          .setCustomId(`download_${i + 1}`)
+          .setLabel(`Assistir ${numbersEmojis[i]}`)
+          .setStyle(ButtonStyle.Success)
+      )
+    }
+
+    if (totalItems === 0) {
+      const embed = new EmbedBuilder()
+        .setTitle(`**__Resultados da Pesquisa__**: ${search}`)
+        .setDescription('Nenhum resultado encontrado')
+        .setColor(client.color.red)
+
+      return ctx.sendMessage({ embeds: [embed] })
+    } else if (totalItems <= 5) {
       const message = await ctx.sendMessage({
         embeds: [initialEmbed],
         components: [initialRow, initialDownloadRow],
@@ -267,8 +278,6 @@ export default class MSearch extends Command {
       collector.on('end', async () => {
         await message.edit({ components: [] })
       })
-    } catch (error) {
-      console.log(`error in msearch command: ${error}`)
     }
   }
 }
