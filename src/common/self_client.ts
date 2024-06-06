e5aa39256cf7c47f9bb9b42b5e1a95f5b047c4a2
Gabriel Maia <gabrielmaialva33@gmail.com>
2024-06-06 04:39:48 -0300

    refactor: self client to ultrafast mode

--- src/common/self_client.ts
+++ src/common/self_client.ts
@@ -109,7 +109,7 @@ export class SelfClient extends Client {
     const streamLinkUdpConn = await this.streamer.createStream({
       width: 1280,
       height: 720,
-      //h26xPreset: 'faster',
+      h26xPreset: 'ultrafast',
     })
 
     this.playVideo(link, streamLinkUdpConn)
